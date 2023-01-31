947d1c4a434124c4ba5c73ed5cf95137ca4e0aca
Juan <juan.giana8589@gmail.com>
2023-01-31 09:17:50 -0600

    added group-sizes.js

--- group-sizes.js
+++ group-sizes.js
@@ -1,25 +1,22 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-let students = Number(prompt("Enter the number of students: "));
+let students = Number(prompt("Enter class size: "));
 console.log(`number of students ${students}`);
 
-let div3 = Math.floor(students/3);
-console.log(`${students} divided by 3 is ${div3}`)
+let div3 = Math.floor(students / 3);
+console.log(`${students} divided by 3 is ${div3}`);
 
 let oddNum = students % 3;
 console.log(`modulus 3 is ${oddNum}`);
 
-let div2 = Math.floor(students/2);
-console.log(`${students} divided by 2 is ${div2}`)
+let div2 = Math.floor(students / 2);
+console.log(`${students} divided by 2 is ${div2}`);
 
 let evenNum = students % 2;
 console.log(`modulus 2 is ${evenNum}`);
 
-
-
-if(students % 2 === 1){
-    console.log(`create ${div3} groups of 3 and ${evenNum} groups of 2`)
-} else if (students / 3 === students / 3 ){
-    console.log(`create ${div3} groups of 3 and ${oddNum} of 2`)
-} else if (students % 2 === 0){}
-
+if (students % 3 === 0) {
+  console.log(`create ${div3} groups of 3 and ${oddNum} groups of 2`);
+} else if (students % 3 === 1) {
+  console.log(`create ${div3} groups of 3 and ${evenNum} groups of 2`);
+}
